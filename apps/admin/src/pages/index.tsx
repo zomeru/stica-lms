947d1abb1dae1a90b2637ce4b7901f247020732c
zomeru <zomerzxc.19@gmail.com>
2023-01-21 00:22:04 +0800

    chore: some tweaks

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -175,7 +175,7 @@ const Home: NextPage = () => {
         {page === 'renewal requests' && <RenewalRequest />}
         {page === 'lost books' && <LostBooks allBooks={allBooks} />}
         {page === 'damaged books' && <DamagedBooks allBooks={allBooks} />}
-        {page === 'archived books' && <Archived allBooks={allBooks} />}
+        {page === 'archived books' && <Archived />}
         {page === 'reports' && <History />}
       </>
     );
